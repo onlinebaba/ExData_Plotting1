159913a266802c66760714165dbd859aef75eead
Ramesh Nori <ramesh@usamp.com>
2014-07-12 14:24:31 -0700

    Initial Checkin of plot1.R and plot1.png

---
+++ plot1.R
@@ -0,0 +1,19 @@
+library(data.table)
+
+## Read the file into a data frame
+plot1DF <- read.table("household_power_consumption.txt", sep=";",header=TRUE, na.strings="?")
+
+## Cast Date column values as Date
+plot1DF$Date <- as.Date(plot1DF$Date,"%d/%m/%Y")
+
+## Subset the DF with the desired date range
+plot1DF <- subset(plot1DF,plot1DF$Date == "2007-2-1" | plot1DF$Date == "2007-2-2")
+
+## Create histogram on a scren device
+with(plot1DF,hist(plot1DF$Global_active_power,xlab="Global Active Power (kilowatts)", col="red",main="Global Active Power"))
+
+## Copy the plot to a PNG file
+dev.copy(png,file="plot1.png")
+
+## Close the device
+dev.off()
