7a733460315224f12c9bcb2b736f9f3f7d81fba6
Ramesh Nori <ramesh@usamp.com>
2014-07-12 16:05:56 -0700

    Check-in plot1.R, plot3.R, plot3.png

--- plot1.R
+++ plot1.R
@@ -11,7 +11,7 @@ plot1DF$Date <- as.Date(plot1DF$Date,"%d/%m/%Y")
 ## Subset the DF with the desired date range
 plot1DF <- subset(plot1DF,plot1DF$Date == "2007-2-1" | plot1DF$Date == "2007-2-2")
 
-## Create histogram on a scren device
+## Create line chart on a scren device
 with(plot1DF,hist(plot1DF$Global_active_power,xlab="Global Active Power (kilowatts)", col="red",main="Global Active Power"))
 
 ## Copy the plot to a PNG file
---
+++ plot3.R
@@ -0,0 +1,28 @@
+library(data.table)
+
+Sys.setlocale()
+
+## Read the file into a data frame
+plot3DF <- read.table("../household_power_consumption.txt", sep=";",header=TRUE, na.strings="?")
+
+## Cast Date column values as Date
+plot3DF$Date <- as.Date(plot3DF$Date,"%d/%m/%Y")
+
+## Subset the DF with the desired date range
+plot3DF <- subset(plot3DF,plot3DF$Date == "2007-2-1" | plot3DF$Date == "2007-2-2")
+xAxisDays <- strptime( paste(plot3DF$Date,plot3DF$Time), format="%Y-%m-%d %H:%M:%S")
+
+## Create line chart at a time on a screen device for each sub metering type
+plot(xAxisDays, plot3DF$Sub_metering_1, ylab="Energy Sub Metering", xlab="", type="n")
+points(xAxisDays, plot3DF$Sub_metering_1, type="l", col="black")
+points(xAxisDays, plot3DF$Sub_metering_2, type="l", col="red")
+points(xAxisDays, plot3DF$Sub_metering_3, type="l", col="blue")
+
+# Add Legend
+legend("topright", lty= "solid", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"))
+
+## Copy the plot to a PNG file
+dev.copy(png,file="figure/plot3.png",width=480,height=480)
+
+## Close the device
+dev.off()
