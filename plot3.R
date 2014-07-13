c7505682b3f1439e4c8e000f556d1ae9300aae6d
onlinebaba <onlinebaba@users.noreply.github.com>
2014-07-13 14:07:43 -0700

    Final comments

--- plot3.R
+++ plot3.R
@@ -3,7 +3,7 @@ library(data.table)
 Sys.setlocale()
 
 ## Read the file into a data frame
-#plot3DF <- read.table("../household_power_consumption.txt", sep=";",header=TRUE, na.strings="?")
+plot3DF <- read.table("../household_power_consumption.txt", sep=";",header=TRUE, na.strings="?")
 
 ## Cast Date column values as Date
 plot3DF$Date <- as.Date(plot3DF$Date,"%d/%m/%Y")
